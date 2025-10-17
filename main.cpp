a8668bc6aab34774fac8fdeeb085ce12d40c70a1
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-10-17 12:07:47 +0500

    Task 4

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 int main() {
-    int number, factorial;
-    factorial = 1;
-    cout << "Enter a number you want to factorial: ";
-    cin >> number;
-    
-    for (int i = number; i > 0; i--) {
-        factorial *= i;
+    int rows = 5;
+    int numToPrint = 1;
+    for (int i = 0; i < rows; i++) {
+        int spaces = rows - i - 1;
+        for (int j = 0; j < spaces; j++) {
+            cout << " ";
+        }
+        int centreNum = (numToPrint / 2) + 1;
+        for (int k = 1; k <= centreNum; k++) {
+            cout << k;
+        }
+        for (int k = centreNum - 1; k > 0; k--) {
+            cout << k;
+        }
+        cout << "\n";
+        numToPrint += 2;
     }
-    cout << factorial;
 }
+
