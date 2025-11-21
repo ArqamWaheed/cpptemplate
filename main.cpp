359d375fd4fd2a648f9f68ee05273339fc755355
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:23:01 +0500

    Task 4 non duplicate values

--- main.cpp
+++ main.cpp
@@ -2,42 +2,34 @@
 using namespace std;
 
 int main() {
-    int set1[10];
-    cout << "Enter 10 numbers for set 1:" << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << "Number " << (i + 1) << ": ";
-        cin >> set1[i];
-    }
-
-    int set2[10];
-    cout << "Enter 10 numbers for set 2:" << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << "Number " << (i + 1) << ": ";
-        cin >> set2[i];
-    }
-
-    cout << "Common elements: ";
-    for (int i = 0; i < 10; i++) {
-        bool found = false;
-        for (int j = 0; j < 10; j++) {
-            if (set1[i] == set2[j]) {
-                found = true;
+    int numbers[20];
+    int count = 0;
+    
+    cout << "Enter 20 integers between 10 and 100:" << endl;
+    for (int i = 0; i < 20; i++) {
+        int num;
+        cin >> num;
+        
+        bool isDuplicate = false;
+        for (int j = 0; j < count; j++) {
+            if (numbers[j] == num) {
+                isDuplicate = true;
                 break;
             }
         }
-        if (found) {
-            bool already = false;
-            for (int k = 0; k < i; k++) {
-                if (set1[k] == set1[i]) {
-                    already = true;
-                    break;
-                }
-            }
-            if (!already) {
-                cout << set1[i] << " ";
-            }
+        
+        if (!isDuplicate) {
+            numbers[count] = num;
+            count++;
         }
     }
+    
     cout << endl;
+    cout << "The nonduplicate values are:" << endl;
+    for (int i = 0; i < count; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
