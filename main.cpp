857ff141dae89c2449f2e4c3f0511bb0958bd7d2
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:16:11 +0500

    Task 2 largest number from 10 array elements with its location

--- main.cpp
+++ main.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 
 int main() {
-    int arr[10];
-    int sum = 0;
-    cout << "Enter 10 numbers: " << endl;
+    int numbers[10];
+    cout << "Enter 10 numbers:" << endl;
     for (int i = 0; i < 10; i++) {
         cout << "Number " << (i + 1) << ": ";
-        cin >> arr[i];
-        sum += arr[i];
+        cin >> numbers[i];
     }
 
-    float average = sum / 10;
-    cout << "Average is: " << average;   
-}
+    int largest = numbers[0];
+    int location = 0;
+    for (int i = 1; i < 10; i++) {
+        if (numbers[i] > largest) {
+            largest = numbers[i];
+            location = i;
+        }
+    }
 
+    cout << "Largest number is: " << largest << endl;
+    cout << "Location: " << (location + 1) << endl;
+    return 0;
+}
