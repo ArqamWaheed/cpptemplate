d7277a1daeb111d689dc2d9904befe873a82a4f5
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:31:21 +0500

    Finished dont open this commit

--- main.cpp
+++ main.cpp
@@ -3,36 +3,34 @@
 using namespace std;
 
 int main() {
-    char arr[10];
-    int lowercase = 0;
-    int uppercase = 0;
-    int digit = 0;
-    int special = 0;
+    char characters[10];
+    int lowerCount = 0;
+    int upperCount = 0;
+    int digitCount = 0;
+    int specialCount = 0;
     
     cout << "Enter 10 characters (mix of lower, upper, digit, special):" << endl;
     for (int i = 0; i < 10; i++) {
-        cin >> arr[i];
-    }
-    
-    for (int i = 0; i < 10; i++) {
-        if (islower(arr[i])) {
-            lowercase++;
+        cin >> characters[i];
+        
+        if (characters[i] >= 'a' && characters[i] <= 'z') {
+            lowerCount++;
         }
-        else if (isupper(arr[i])) {
-            uppercase++;
+        else if (characters[i] >= 'A' && characters[i] <= 'Z') {
+            upperCount++;
         }
-        else if (isdigit(arr[i])) {
-            digit++;
+        else if (characters[i] >= '0' && characters[i] <= '9') {
+            digitCount++;
         }
         else {
-            special++;
+            specialCount++;
         }
     }
     
-    cout << "Lower case = " << lowercase << endl;
-    cout << "Upper case = " << uppercase << endl;
-    cout << "Digit = " << digit << endl;
-    cout << "Special characters = " << special << endl;
+    cout << "Lower case = " << lowerCount << endl;
+    cout << "Upper case = " << upperCount << endl;
+    cout << "Digit = " << digitCount << endl;
+    cout << "Special characters = " << specialCount << endl;
     
     return 0;
 }
