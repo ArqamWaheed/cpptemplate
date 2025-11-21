1bf3257d420f5b2b27d426de7339e8795c6c09db
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:00:03 +0500

    Week 10 FOCP lab start

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Input: ";
-    cin >> n;
     
-    cout << "Output:" << endl;
-    
-    for (int i = 1; i <= 10; i++) {
-        cout << n << " x " << i << " = " << n * i << endl;
-    }
 }
 
