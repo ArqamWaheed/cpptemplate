2c0eff994fa7bb6691d61caa44c4fdd645552f2d
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:18:03 +0500

    Task 3 common intersection of two arrays

--- main.cpp
+++ main.cpp
@@ -2,23 +2,42 @@
 using namespace std;
 
 int main() {
-    int numbers[10];
-    cout << "Enter 10 numbers:" << endl;
+    int set1[10];
+    cout << "Enter 10 numbers for set 1:" << endl;
     for (int i = 0; i < 10; i++) {
         cout << "Number " << (i + 1) << ": ";
-        cin >> numbers[i];
+        cin >> set1[i];
     }
 
-    int largest = numbers[0];
-    int location = 0;
-    for (int i = 1; i < 10; i++) {
-        if (numbers[i] > largest) {
-            largest = numbers[i];
-            location = i;
+    int set2[10];
+    cout << "Enter 10 numbers for set 2:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Number " << (i + 1) << ": ";
+        cin >> set2[i];
+    }
+
+    cout << "Common elements: ";
+    for (int i = 0; i < 10; i++) {
+        bool found = false;
+        for (int j = 0; j < 10; j++) {
+            if (set1[i] == set2[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            bool already = false;
+            for (int k = 0; k < i; k++) {
+                if (set1[k] == set1[i]) {
+                    already = true;
+                    break;
+                }
+            }
+            if (!already) {
+                cout << set1[i] << " ";
+            }
         }
     }
-
-    cout << "Largest number is: " << largest << endl;
-    cout << "Location: " << (location + 1) << endl;
+    cout << endl;
     return 0;
 }
