5b19d22afb6c891b4b9e8d0f3d14463f405bf5dc
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-21 12:12:34 +0500

    Task 1 average of 10 array elements

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-    
+    int arr[10];
+    int sum = 0;
+    cout << "Enter 10 numbers: " << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Number " << (i + 1) << ": ";
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    float average = sum / 10;
+    cout << "Average is: " << average;   
 }
 
